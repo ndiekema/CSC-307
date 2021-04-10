6e0a5443a74ee0ad510c6999fbe630b7d932de04
Nathan Diekema <ndiekema@calpoly.edu>
2021-04-09 18:50:49 -0700

    Added remove buttons

--- src/MyApp.js
+++ src/MyApp.js
@@ -1,7 +1,8 @@
-import React from 'react'
-import Table from './Table'
+import React, {useState} from 'react';
+import Table from './Table';
 
-const characters = [
+function MyApp() {
+    const [characters, setCharacters] = useState([
       {
         name: 'Charlie',
         job: 'Janitor',
@@ -18,16 +19,20 @@ const characters = [
         name: 'Dennis',
         job: 'Bartender',
       },
-];
+    ]);
 
-function MyApp() {
+    function removeOneCharacter (index) {
+      const updated = characters.filter((character, i) => {
+          return i !== index
+        });
+        setCharacters(updated);
+    }
+
     return (
-      <div className="container">
-        <Table characterData={characters} />
-      </div>
-    );
+    <div className="container">
+      <Table characterData={characters} removeCharacter={removeOneCharacter} />
+    </div>
+    )
 }
-
-
 
 export default MyApp;
--- src/Table.js
+++ src/Table.js
@@ -6,6 +6,7 @@ function TableHeader()  {
       <tr>
         <th>Name</th>
         <th>Job</th>
+        <th>Remove</th>
       </tr>
     </thead>
   );
@@ -15,8 +16,11 @@ function TableBody(props) {
   const rows = props.characterData.map((row, index) => {
     return (
       <tr key={index}>
-        <td>{row.name}</td>         
+        <td>{row.name}</td>
         <td>{row.job}</td>
+        <td>
+          <button onClick={() => props.removeCharacter(index)}>Delete</button>
+        </td>
       </tr>
     );
    }
@@ -28,13 +32,13 @@ function TableBody(props) {
    );
 }
 
-function Table(props) {
-    return (
-      <table>
-        <TableHeader />
-        <TableBody characterData={props.characterData} />
-      </table>
-    );
+function Table (props) {
+  return (
+    <table>
+      <TableHeader />
+      <TableBody characterData={props.characterData} removeCharacter={props.removeCharacter} />
+    </table>
+  );
 }
 
 export default Table;
