cc7196e2bbcde00b43fe94ae5b8abe2d4f4cccec
Nathan Diekema <ndiekema@calpoly.edu>
2021-04-11 20:11:47 -0700

    Added our first Axiods code in frontend

--- src/MyApp.js
+++ src/MyApp.js
@@ -1,21 +1,42 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import Table from './Table';
 import Form from './Form';
+import axios from 'axios';
 
 function MyApp() {
     const [characters, setCharacters] = useState([]);
 
     function removeOneCharacter (index) {
-      const updated = characters.filter((character, i) => {
-          return i !== index
+        const updated = characters.filter((character, i) => {
+            return i !== index
         });
         setCharacters(updated);
     }
 
     function updateList(person) {
-      setCharacters([...characters, person]);
+        setCharacters([...characters, person]);
     }
-    
+
+    // return the data which we'll use to mount our table to the frontend
+    async function fetchAll(){
+       try {
+          const response = await axios.get('http://localhost:5000/users');
+          return response.data.users_list;     
+       }
+       catch (error) {
+          //We're not handling errors. Just logging into the console.
+          console.log(error); 
+          return false;         
+       }
+    }
+
+    useEffect(() => {
+        fetchAll().then( result => {
+            if (result)
+                setCharacters(result);
+        });
+    }, [] );
+
     return (
     <div className="container">
       <Table characterData={characters} removeCharacter={removeOneCharacter} />
